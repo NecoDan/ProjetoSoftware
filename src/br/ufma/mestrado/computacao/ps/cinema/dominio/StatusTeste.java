22d29a285ecaceffaac4f8bc1046cea2a7064028
Daniel <daniel.goncalves@armateus.com.br>
2018-03-02 19:57:10 -0300

    teste valendo jenkins quero 10

---
+++ src/br/ufma/mestrado/computacao/ps/cinema/dominio/StatusTeste.java
@@ -0,0 +1,15 @@
+package br.ufma.mestrado.computacao.ps.cinema.dominio;
+
+public class StatusTeste {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
